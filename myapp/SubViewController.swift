e5f61146459b8b84b7c2e1584e52f97ca18578a6
pipinosuke <ys.pipinosuke@gmail.com>
2018-06-09 14:23:14 +0900

    たぶ二つの切り替え

---
+++ myapp/SubViewController.swift
@@ -0,0 +1,52 @@
+//
+//  SubViewController.swift
+//  myapp
+//
+//  Created by pipinosuke on 2018/06/09.
+//  Copyright © 2018年 pipinosuke. All rights reserved.
+//
+
+import UIKit
+
+class SubViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+//extension SubViewController: UITableViewDelegate {
+//
+//}
+//
+//extension SubViewController: UITableViewDataSource {
+//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return 1
+//    }
+//
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        <#code#>
+//    }
+//
+//}
