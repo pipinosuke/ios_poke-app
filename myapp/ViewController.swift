b7bda69e0c5b1bccea8d1ab7d28377afa9944153
pipinosuke <ys.pipinosuke@gmail.com>
2018-06-09 13:53:02 +0900

    Initial Commit

---
+++ myapp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  myapp
+//
+//  Created by pipinosuke on 2018/06/09.
+//  Copyright © 2018年 pipinosuke. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
