0b31aad38bd3d441428bd8470b31850a891c3494
pipinosuke <ys.pipinosuke@gmail.com>
2018-07-04 00:44:28 +0900

    pokeviewmodelコミットし忘れ

---
+++ myapp/ViewModel/PokeViewModel.swift
@@ -0,0 +1,17 @@
+//
+//  PokeViewModel.swift
+//  myapp
+//
+//  Created by pipinosuke on 2018/07/02.
+//  Copyright © 2018年 pipinosuke. All rights reserved.
+//
+
+import APIKit
+import BrightFutures
+
+class PokeViewModel {
+    var pokemon =  Pokemon()
+    func fetchPoke(params: [String: Any]) -> Future<GetPokeRequest.Response, SessionTaskError > {
+        return APIManager.send(request: GetPokeRequest(queryParameters: params))
+    }
+}
